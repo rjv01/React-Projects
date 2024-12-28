d2eaef211cdd2a073a23635b0f903319e0aaec50
rjv01 <verma.rajshekhar1@gmail.com>
2024-12-28 18:27:09 +0530

    adding files

---
+++ nasa-react-app/src/App.jsx
@@ -0,0 +1,58 @@
+import { useEffect, useState } from "react";
+import Footer from "./components/Footer";
+import Main from "./components/Main";
+import SideBar from "./components/SideBar";
+
+function App() {
+  const [data, setData] = useState(null);
+  const [showModal, setShowModal] = useState(false);
+
+  function handleToggleModal() {
+    setShowModal(!showModal);
+  }
+
+  useEffect(() => {
+    async function fetchAPIData() {
+      const NASA_KEY = import.meta.env.VITE_NASA_API_KEY;
+      const url = `https://api.nasa.gov/planetary/apod?api_key=${NASA_KEY}`;
+      // console.log("Fetching data from: ", url);
+
+      const today = new Date().toDateString();
+      const localKey = `NASA-${today}`;
+      if (localStorage.getItem(localKey)) {
+        const apiData = JSON.parse(localStorage.getItem(localKey));
+        setData(apiData);
+        console.log("Fetched from cache today: ", apiData);
+        return;
+      }
+      localStorage.clear();
+
+      try {
+        const res = await fetch(url);
+        const apiData = await res.json();
+        // console.log("API Data: ", apiData); // Debugging API response
+        localStorage.setItem(localKey, JSON.stringify(apiData));
+        setData(apiData);
+      } catch (err) {
+        console.log("Error fetching data: ", err.message);
+      }
+    }
+    fetchAPIData();
+  }, []);
+
+  return (
+    <>
+      {data ? (
+        <Main data={data} />
+      ) : (
+        <div className="loadingState">
+          <i className="fa-solid fa-gear"></i>
+        </div>
+      )}
+      {showModal && <SideBar data={data} handleToggleModal={handleToggleModal} />}
+      {data && <Footer data={data} handleToggleModal={handleToggleModal} />}
+    </>
+  );
+}
+
+export default App;
