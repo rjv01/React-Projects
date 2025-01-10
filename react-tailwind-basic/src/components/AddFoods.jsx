aabf73f32f74975c6e2de835ad772d41d91afc12
rjv01 <verma.rajshekhar1@gmail.com>
2025-01-10 18:48:55 +0530

    updating addfood file

--- react-tailwind-basic/src/components/AddFoods.jsx
+++ react-tailwind-basic/src/components/AddFoods.jsx
@@ -1,36 +1,52 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react';
 
 export default function AddFoods() {
-    const [foods,setFoods] = useState(["Apple","Pijjo","Cake"]);
-    
-    function handleAddFood(e){
-        const newFood = document.getElementById("inputfood").value;
-        if(!newFood==""){
-            setFoods(f=>([...f,newFood]));
-        }
-        else
+    const [foods, setFoods] = useState(["Apple", "Pijjo", "Cake"]);
+    const inputRef = useRef(null);
+
+    function handleAddFood() {
+        const newFood = inputRef.current.value.trim();
+        if (newFood !== "") {
+            setFoods((prevFoods) => [...prevFoods, newFood]);
+        } else {
             alert("Enter Some Value");
-        document.getElementById("inputfood").value="";
-    }
-    function handleRemoveFood(index){
-        setFoods(foods.filter((_,i)=> i!== index));
+        }
+        inputRef.current.value = "";
     }
 
-  return (
-    <div className='min-h-screen flex flex-col justify-center items-center bgGrid'>
-        <h1 className='text-5xl m-5'>Food Items</h1>
-        <p className='text-lg underline m-5'>Click on items to remove it</p>
-        <ul className='text-3xl'>
-            {foods.map((food,index)=>
-                <li className='flex flex-col justify-center items-center hover:bg-red-400 border rounded-lg w-[120px] h-[50px] m-5' key={index} onClick={()=> handleRemoveFood(index)}>
-                        {food} 
-                        </li>
-            )}
-        </ul>
-        <div className='m-5 text-xl'>
-            <input className='m-5 w-[250px] h-[40px]' type="text" name="" id="inputfood" placeholder='Enter Food Element'/>
-            <button className=' border rounded-full w-[100px] h-[50px] bg-blue-400 hover:bg-blue-600 duration-200' onClick={handleAddFood}>Add</button>
+    function handleRemoveFood(index) {
+        setFoods((prevFoods) => prevFoods.filter((_, i) => i !== index));
+    }
+
+    return (
+        <div className="min-h-screen flex flex-col justify-center items-center bgGrid">
+            <h1 className="text-5xl m-5">Food Items</h1>
+            <p className="text-lg underline m-5">Click on items to remove it</p>
+            <ul className="text-3xl">
+                {foods.map((food, index) => (
+                    <li
+                        key={index}
+                        className="flex flex-col justify-center items-center hover:bg-red-400 border rounded-lg w-[120px] h-[50px] m-5"
+                        onClick={() => handleRemoveFood(index)}
+                    >
+                        {food}
+                    </li>
+                ))}
+            </ul>
+            <div className="m-5 text-xl">
+                <input
+                    ref={inputRef}
+                    className="m-5 w-[250px] h-[40px]"
+                    type="text"
+                    placeholder="Enter Food Element"
+                />
+                <button
+                    className="border rounded-full w-[100px] h-[50px] bg-blue-400 hover:bg-blue-600 duration-200"
+                    onClick={handleAddFood}
+                >
+                    Add
+                </button>
+            </div>
         </div>
-    </div>
-  )
+    );
 }
